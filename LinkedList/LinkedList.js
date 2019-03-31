8edb46d15a0db5f694ff5610368081959a547588
whbxyr <whbxyr@gmail.com>
2019-03-31 20:47:11 +0800

    添加链表相关文件

---
+++ LinkedList/LinkedList.js
@@ -0,0 +1,19 @@
+/**
+ * @File: 数据结构之链表
+ * @Author: xuyuanrui
+ * @Email: raywhbxyr@163.com
+ * @Date: 2019-03-26 13:29:43
+ **/
+
+// 结点数据结构
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = null
+  }
+}
+
+class LinkedList {
+  lenght = 0
+  head = null
+}
