68b14e98653c5ed6697e7326d291d6db8578b653
whbxyr <whbxyr@gmail.com>
2019-04-06 17:12:06 +0800

    完善链表接口

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -14,6 +14,92 @@ class Node {
 }
 
 class LinkedList {
-  lenght = 0
+  length = 0
   head = null
+
+  append(element) {
+    const node = new Node(element)
+    if (this.head === null) {
+      this.head = node
+    } else {
+      let current = head
+      while (current.next) {
+        current = current.next
+      }
+      current.next = node
+    }
+    this.length++
+  }
+
+  removeAt(pos) {
+    if (pos > -1 && pos < this.length) {
+      let current = this.head
+      if (pos === 0) {
+        this.head = current.next
+      } else {
+        let index = 0, prev = current
+        while (index++ < pos) {
+          prev = current
+          current = current.next
+        }
+        prev.next = current.next
+      }
+      this.length--
+      return current.element
+    }
+    return null
+  }
+
+  insert(pos, element) {
+    if (pos > -1 && pos < this.length) {
+      let current = this.head
+      let node = new Node(element)
+      if (pos === 0) {
+        node.next = current
+        this.head = node
+      } else {
+        let prev = current, index = 0
+        while (index++ < pos) {
+          prev = current
+          current = current.next
+        }
+        prev.next = node
+        node.next = current
+      }
+      this.length++
+      return true
+    }
+    return false
+  }
+
+  indexOf(element) {
+    let current = this.head, index = -1
+    while (current) {
+      index++
+      if (current.element === element) {
+        return index
+      }
+      current = current.next
+    }
+    return -1
+  }
+
+  remove(element) {
+    const pos = this.indexOf(element)
+    return this.removeAt(pos)
+  }
+
+  isEmpty() {
+    return this.length === 0
+  }
+
+  size() {
+    return this.length
+  }
+
+  getHead() {
+    return this.head
+  }
 }
+
+module.exports = LinkedList
